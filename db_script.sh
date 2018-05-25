b3304cb7372b5b3285e901113d78af4612eb8278
Zaire Paterson <zpaterson@Zaires-MacBook-Pro.local>
2018-05-25 12:30:24 -0700

    added create and teardown bash script files

--- db_script.sh
+++ db_script.sh
@@ -1,17 +1,28 @@
-# psql workoutTime
-create database workout_time;
+#!/bin/bash
+set -e # Flag causes script to exit if there's an error
 
-CREATE TABLE users (
+# Set variables
+database_name='workout_time'
+table1_name='users'
+table2_name='preferences'
+
+
+echo Creating database $database_name...
+psql -c "CREATE DATABASE $database_name"
+
+echo Creating table $table1_name...
+psql -d $database_name -c "CREATE TABLE $table1_name(
 	id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(40)
-);
+);"
 
 
-CREATE TABLE preferences (
+echo Creating table $table2_name...
+psql -d $database_name -c "CREATE TABLE $table2_name(
 	id SERIAL PRIMARY KEY NOT NULL,
 	user_id SERIAL REFERENCES users(id),
 	time_of_day VARCHAR(40),
 	days_per_week INTEGER,
 	hours_per_week INTEGER,
-	date_created TIMESTAMP WITH TIMEZONE
-);
+	date_created TIMESTAMP WITH TIME ZONE
+);"
---
+++ teardown_db_sample.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set variables
+database_name='tester_db'
+table1_name='users'
+table2_name='preferences'
+
+read -p "Are you sure you want to drop table $table2_name? Reply y or n " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   echo Dropping table $table2_name...
+    psql -d $database_name -c "DROP TABLE $table2_name"
+fi
+
+read -p "Are you sure you want to drop table $table1_name? Reply y or n " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   echo Dropping table $table1_name...
+    psql -d $database_name -c "DROP TABLE $table1_name"
+fi
+
+
+read -p "Are you sure you want to drop table $database_name? Reply y or n " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   echo Dropping database $database_name...
+    psql -c "DROP DATABASE $database_name"
+fi
