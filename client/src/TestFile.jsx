1f09ad23abce84781b11fb5489fb87e807fd3a52
Zaire Paterson <zzpaterson@gmail.com>
2021-05-29 18:15:19 -0400

    added test file

---
+++ client/src/TestFile.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class TestFile extends Component {
+    render() {
+        return(
+            <div> auidyusyufdsyfiudyuifdsyufdysufyfdsiuydsiuyfdsuiyfdusi</div>
+        );
+    }
+}
